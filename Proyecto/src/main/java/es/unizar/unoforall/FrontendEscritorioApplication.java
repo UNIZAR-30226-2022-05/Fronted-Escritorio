0ceb84f5e38cbfee32ea794e00affdcb47872b60
Raúl Iranzo <mail.rauliranzo@gmail.com>
2022-03-17 17:16:06 +0100

    Proyecto
    He creado el proyecto de Eclipse

---
+++ Proyecto/src/main/java/es/unizar/unoforall/FrontendEscritorioApplication.java
@@ -0,0 +1,13 @@
+package es.unizar.unoforall;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FrontendEscritorioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FrontendEscritorioApplication.class, args);
+	}
+
+}
