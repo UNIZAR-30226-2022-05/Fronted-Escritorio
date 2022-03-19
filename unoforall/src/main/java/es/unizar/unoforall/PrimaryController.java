1aadaf19a5906e9cb9a8c9efcd0ac3776bbf1e56
Gabinsito <800128@unizar.es>
2022-03-19 22:09:38 +0100

    login funcional
    v1

--- unoforall/src/main/java/es/unizar/unoforall/PrimaryController.java
+++ unoforall/src/main/java/es/unizar/unoforall/PrimaryController.java
@@ -1,12 +1,49 @@
 package es.unizar.unoforall;
 
 import java.io.IOException;
+
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.text.TextAlignment;
 
-public class PrimaryController {
+public class PrimaryController {	
+	@FXML private Label labelInformacion;
 
+	@FXML private TextField cajaCorreo;
+	@FXML private Label labelCorreo;
+	
+	@FXML private TextField cajaContrasenya;
+	@FXML private Label labelContrasenya;
+	
     @FXML
     private void switchToSecondary() throws IOException {
         App.setRoot("secondary");
     }
+    
+    @FXML
+    private void checkLogin(ActionEvent event) {
+    	
+    	//Mandar datos a la BD
+    	
+    	//Verificar resultado de la llamada
+    	
+    	//Chequeo provisional
+
+		try {
+	    	if (cajaCorreo.getText().equals("a@unizar") && cajaContrasenya.getText().equals("1234")) {
+	    		labelInformacion.setText("Correo y contraseña correctos. Aplicación incorrecta");
+				switchToSecondary();
+	    	} else {
+	    		labelInformacion.setText("Correo y contraseña incorrectos. Pruebe otra cosa");
+	    		labelInformacion.setTextAlignment(TextAlignment.CENTER);
+	    		cajaCorreo.setText("");
+	    		cajaContrasenya.setText("");
+	    	}
+		}
+		catch (IOException e) {
+			System.out.print(e);
+		}
+	}
 }
--- unoforall/src/main/java/module-info.java
+++ unoforall/src/main/java/module-info.java
@@ -2,6 +2,7 @@ module es.unizar.unoforall {
     requires javafx.controls;
     requires javafx.fxml;
     requires transitive javafx.graphics;
+	requires javafx.base;
 
     opens es.unizar.unoforall to javafx.fxml;
     exports es.unizar.unoforall;
