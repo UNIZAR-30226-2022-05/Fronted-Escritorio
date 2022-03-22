34bfb642e8a23764fdd1cc00c596611a72bdf6e8
Raúl Iranzo <mail.raulic@gmail.com>
2022-03-22 01:34:04 +0100

    Integración con clase externa WebSockets

--- unoforall/src/main/java/es/unizar/unoforall/App.java
+++ unoforall/src/main/java/es/unizar/unoforall/App.java
@@ -7,40 +7,20 @@ import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 import java.io.IOException;
-//import java.lang.reflect.Type;
-//import java.util.Scanner;
-//import java.util.concurrent.ExecutionException;
-//
-////import org.springframework.messaging.converter.MappingJackson2MessageConverter;
-//import org.springframework.messaging.simp.stomp.StompCommand;
-//import org.springframework.messaging.simp.stomp.StompHeaders;
-//import org.springframework.messaging.simp.stomp.StompSession;
-//import org.springframework.messaging.simp.stomp.StompSessionHandler;
-//import org.springframework.messaging.simp.stomp.StompSession.Subscription;
-//import org.springframework.web.socket.client.WebSocketClient;
-//import org.springframework.web.socket.client.standard.StandardWebSocketClient;
-//import org.springframework.web.socket.messaging.WebSocketStompClient;
 import java.util.concurrent.ExecutionException;
 
 import es.unizar.pruebaCliente.PruebaClienteApplication;
-
-
-
 
 /**
  * JavaFX App
  */
 public class App extends Application {
-	private static Object LOCK = new Object();
-	
+
     private static Scene scene;
 
     @Override
     public void start(Stage stage) throws IOException {
         scene = new Scene(loadFXML("primary"), 640, 480);
-        
-        
-        
         stage.setScene(scene);
         stage.show();
     }
@@ -54,70 +34,13 @@ public class App extends Application {
         return fxmlLoader.load();
     }
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException, ExecutionException {
         launch();
-        try {
-			test();
-		} catch (InterruptedException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (ExecutionException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
+        test();
     }
     
-   public static void test() throws InterruptedException, ExecutionException {
-	   PruebaClienteApplication.main(null);
-	   
-//		WebSocketClient client = new StandardWebSocketClient();
-//
-//		WebSocketStompClient stompClient = new WebSocketStompClient(client);
-//		stompClient.setMessageConverter(new MappingJackson2MessageConverter());
-//
-//		StompSessionHandler sessionHandler = new StompSessionHandler() {
-//			@Override
-//			public void afterConnected(StompSession session, StompHeaders connectedHeaders) {
-//				synchronized (LOCK) {
-//					LOCK.notify();
-//				}
-//			}
-//			@Override
-//			public void handleFrame(StompHeaders headers, Object payload) {
-//				String e = (String) payload;
-//			    System.out.println(e);
-//			}
-//			@Override
-//			public Type getPayloadType(StompHeaders headers) {
-//				return String.class;
-//			}
-//			@Override
-//			public void handleException(StompSession session, StompCommand command, StompHeaders headers,
-//					byte[] payload, Throwable exception) {
-//				// TODO Auto-generated method stub
-//				
-//			}
-//			@Override
-//			public void handleTransportError(StompSession session, Throwable exception) {
-//				// TODO Auto-generated method stub
-//				
-//			}
-//		};
-//		StompSession sesion = stompClient.connect("ws://localhost/gs-guide-websocket", sessionHandler).get();
-//		
-//		while(!sesion.isConnected()) {
-//			synchronized (LOCK) {
-//				LOCK.wait();
-//			}
-//		}
-//
-//	    Subscription s = sesion.subscribe("/topic/greetings", sessionHandler);
-//	    sesion.send("/app/hello", "buenos dias");
-//	    System.out.println("mensaje enviado");
-//	    
-//		new Scanner(System.in).nextLine(); // Don't close immediately.
-//		
-//		sesion.disconnect();
-   }
+    public static void test() throws InterruptedException, ExecutionException {
+    	PruebaClienteApplication.main(null);
+    }
 
 }
