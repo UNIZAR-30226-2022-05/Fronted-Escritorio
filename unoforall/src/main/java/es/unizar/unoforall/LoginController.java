48a312d7311083057b2be2bfc1cf666c55a3b0d2
ivanp23 <ivanpla2001@gmail.com>
2022-06-05 16:22:51 +0200

    Actualizado LoginController.java

--- unoforall/src/main/java/es/unizar/unoforall/LoginController.java
+++ unoforall/src/main/java/es/unizar/unoforall/LoginController.java
@@ -4,11 +4,11 @@ import java.net.URL;
 import java.util.ResourceBundle;
 
 import es.unizar.unoforall.api.RestAPI;
+import es.unizar.unoforall.api.WebSocketAPI;
 import es.unizar.unoforall.model.RespuestaLogin;
 import es.unizar.unoforall.model.UsuarioVO;
 import es.unizar.unoforall.utils.HashUtils;
 import es.unizar.unoforall.utils.ImageManager;
-import es.unizar.unoforall.utils.StringUtils;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
@@ -52,67 +52,44 @@ public class LoginController implements Initializable {
     	String contrasenna = cajaContrasenya.getText();
 
     	///LOGIN
-		RestAPI apirest = new RestAPI("/api/login");
-		apirest.addParameter("correo", correo);
-		apirest.addParameter("contrasenna", HashUtils.cifrarContrasenna(contrasenna));
-		apirest.setOnError(e -> {if (DEBUG) System.out.println(e);});
-    	
-		apirest.openConnection();
-    	RespuestaLogin resp = apirest.receiveObject(RespuestaLogin.class);
-    	
-    	if (resp != null && resp.isExito()) {
-    		//GUARDAR RESPUESTALOGIN EN CASO DE NECESITARLO
-    		App.setRespLogin(resp);
-    		if (DEBUG) System.out.println("clave inicio: " + resp.getClaveInicio());
-    		
-    		//CONEXION
-    		try {
-    			App.apiweb.openConnection();
-    		} catch (Exception e) {
-    			if (DEBUG) e.printStackTrace();
-			}
-	    	
-			App.apiweb.subscribe("/topic/conectarse/" + resp.getClaveInicio(), String.class, s -> {
-	    		if (s == null) {
-	    			labelError.setText("Error al conectarse al servidor");
-	    			if (DEBUG) System.out.println("Error al conectarse al servidor");
-	    		} else {
-	    			App.setSessionID(s);
-	    			RestAPI apirest2 = new RestAPI("/api/sacarUsuarioVO");
-	    			apirest2.addParameter("sesionID", s);
-	    			apirest2.openConnection();
-	    			
-	    			UsuarioVO usuarioVO = apirest2.receiveObject(UsuarioVO.class);
-	    			
-	    			App.setUsuarioID(usuarioVO.getId());
-	    			if (DEBUG) {
-	    				System.out.println("ID sesión: " + App.getSessionID());
-		    			System.out.println("Sesión iniciada");
-	    			}
-	    			
-	    			//OBTENER ASPECTO DE LA APLICACION
-	    			App.initializePersonalizacion();
-	    			
-	    			//ACTIVAR NOTIFICACIONES
-	    			App.activarNotificaciones();
-	    			
-	    			//ENTRAR A LA APLICACION
-		        	App.setRoot("principal");
-	    		}
-	    	});
-	    	
-			App.apiweb.sendObject("/app/conectarse/" + resp.getClaveInicio(), "vacio");		
-			if (DEBUG) System.out.println("Esperando inicio sesión... ");
-    	} else if (resp != null) {
-    		if (DEBUG) {
-    			labelError.setText(StringUtils.parseString(resp.getErrorInfo()));
-		    	System.out.println("Exito: " + resp.isExito());
-		    	System.out.println("Tipo de error: " + resp.getErrorInfo());
-    		}
-    	} else {
+    	App.apiweb = new WebSocketAPI();
+    	App.apiweb.setOnError(ex -> {
     		labelError.setText("El servidor no es alcanzable");
     		if (DEBUG) System.out.println("El servidor no es alcanzable");
-    	}
+    		ex.printStackTrace();
+    		App.apiweb.close();
+    	});
+    	
+    	App.apiweb.openConnection("/topic");
+    	
+    	RestAPI api = App.apiweb.getRestAPI();
+    	api.addParameter("correo", correo);
+    	api.addParameter("contrasenna", HashUtils.cifrarContrasenna(contrasenna));
+    	api.openConnection("/api/login");
+    	api.receiveObject(RespuestaLogin.class, respuestaLogin -> {
+    		if(respuestaLogin.isExito()) {
+    			RestAPI api2 = App.apiweb.getRestAPI();
+    			api2.openConnection("/api/sacarUsuarioVO");
+    			api2.receiveObject(UsuarioVO.class, usuarioVO -> {
+    				App.setUsuarioID(usuarioVO.getId());
+	    			if (DEBUG) {
+	    				System.out.println("ID sesión: " + App.apiweb.getSessionID());
+		    			System.out.println("Sesión iniciada");
+	    			}
+    			});
+    			
+    			//OBTENER ASPECTO DE LA APLICACION
+    			App.initializePersonalizacion();
+    			
+    			//ACTIVAR NOTIFICACIONES
+    			App.activarNotificaciones();
+    			
+    			//ENTRAR A LA APLICACION
+	        	App.setRoot("principal");
+    		}else {
+    			App.apiweb.close();
+    		}
+    	});
 	}
     
     @FXML
