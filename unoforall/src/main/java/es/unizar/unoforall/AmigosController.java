ad15758568293af7ec2405830e63d68bd446b521
ivanp23 <ivanpla2001@gmail.com>
2022-06-05 16:36:30 +0200

    Actualizado AmigosController.java

--- unoforall/src/main/java/es/unizar/unoforall/AmigosController.java
+++ unoforall/src/main/java/es/unizar/unoforall/AmigosController.java
@@ -72,74 +72,73 @@ public class AmigosController implements Initializable{
 		});
 		
 		//BUSCAR AMIGOS
-		String sesionID = App.getSessionID();
 		
-		RestAPI apirest = new RestAPI("/api/sacarAmigos");
-		apirest.addParameter("sesionID", sesionID);
+		RestAPI apirest = App.apiweb.getRestAPI();
 		apirest.setOnError(e -> {if (DEBUG) System.out.println(e);});
     	
-		apirest.openConnection();
-		ListaUsuarios usuarios = apirest.receiveObject(ListaUsuarios.class);
-		
-		//COMPROBAR SI HA HABIDO ALGÚN ERROR
-		String error = usuarios.getError();
-		if (error.equals("null")) {
-			
-			for (UsuarioVO usuario : usuarios.getUsuarios()) {
-				try {
-        	        FXMLLoader fxmlLoader = new FXMLLoader(App.class.getResource("amigoItem.fxml"));
-    	        	HBox amigoItem = fxmlLoader.load();
-    	        	
-    	        	AmigoItemController amigoItemController = fxmlLoader.getController();
-    	        	amigoItemController.setData(usuario, true);
-    	        	
-        	        listaAmigos.addRow(listaAmigos.getRowCount(), amigoItem);
-        	        listaAmigosLocal.add(usuario);
-        			
-        			if (DEBUG) System.out.println("amigo encontrado:" + usuario.getCorreo());
-    			} catch (IOException e) {
-    				if (DEBUG) e.printStackTrace();
-    			}
+		apirest.openConnection("/api/sacarAmigos");
+		apirest.receiveObject(ListaUsuarios.class, usuarios -> {
+			//COMPROBAR SI HA HABIDO ALGÚN ERROR
+			String error = usuarios.getError();
+			if (error.equals("null")) {
+				
+				for (UsuarioVO usuario : usuarios.getUsuarios()) {
+					try {
+	        	        FXMLLoader fxmlLoader = new FXMLLoader(App.class.getResource("amigoItem.fxml"));
+	    	        	HBox amigoItem = fxmlLoader.load();
+	    	        	
+	    	        	AmigoItemController amigoItemController = fxmlLoader.getController();
+	    	        	amigoItemController.setData(usuario, true);
+	    	        	
+	        	        listaAmigos.addRow(listaAmigos.getRowCount(), amigoItem);
+	        	        listaAmigosLocal.add(usuario);
+	        			
+	        			if (DEBUG) System.out.println("amigo encontrado:" + usuario.getCorreo());
+	    			} catch (IOException e) {
+	    				if (DEBUG) e.printStackTrace();
+	    			}
+				}
+				
+			} else {
+				labelError.setText(StringUtils.parseString(error));
+				if (DEBUG) System.out.println(StringUtils.parseString(error));
 			}
-			
-		} else {
-			labelError.setText(StringUtils.parseString(error));
-			if (DEBUG) System.out.println(StringUtils.parseString(error));
-		}
+		});
+		
 		
 		//CARGAR LISTA DE PETICIONES ENVIADAS
-		apirest = new RestAPI("/api/sacarPeticionesEnviadas");
-		apirest.addParameter("sesionID", App.getSessionID());
+		apirest = App.apiweb.getRestAPI();
 		apirest.setOnError(e -> {
 			if(DEBUG) System.out.println(e);
 			labelError.setText(StringUtils.parseString(e.toString()));
 		});
 		
-		apirest.openConnection();
-    	ListaUsuarios enviadas = apirest.receiveObject(ListaUsuarios.class);
-    	if(enviadas.isExpirado()) {
-    		if(DEBUG) System.out.println("La sesión ha expirado.");
-			labelError.setText("La sesión ha expirado.");
-    	} else if (!enviadas.getError().equals("null")) {
-    		if(DEBUG) System.out.println("Error en peticiones Enviadas: " + StringUtils.parseString(enviadas.getError()));
-			labelError.setText("Error en peticiones Enviadas: " + StringUtils.parseString(enviadas.getError()));
-    	} else {
-    		for (UsuarioVO enviada : enviadas.getUsuarios()) {
-    			try {
-        	        FXMLLoader fxmlLoader = new FXMLLoader(App.class.getResource("peticionItem.fxml"));
-    	        	HBox salaItem = fxmlLoader.load();
-    	        	
-    	        	PeticionItemController peticionItemController = fxmlLoader.getController();
-    	        	peticionItemController.setData(enviada, true);
-    	        	
-    	        	listaEnviadas.addRow(listaEnviadas.getRowCount(), salaItem);
-        			
-        			if (DEBUG) System.out.println("amigo encontrado:" + enviada.getCorreo());
-    			} catch (IOException e) {
-    				if (DEBUG) e.printStackTrace();
-    			}
-    		}
-    	}
+		apirest.openConnection("/api/sacarPeticionesEnviadas");
+    	apirest.receiveObject(ListaUsuarios.class, enviadas -> {
+    		if(enviadas.isExpirado()) {
+	    		if(DEBUG) System.out.println("La sesión ha expirado.");
+				labelError.setText("La sesión ha expirado.");
+	    	} else if (!enviadas.getError().equals("null")) {
+	    		if(DEBUG) System.out.println("Error en peticiones Enviadas: " + StringUtils.parseString(enviadas.getError()));
+				labelError.setText("Error en peticiones Enviadas: " + StringUtils.parseString(enviadas.getError()));
+	    	} else {
+	    		for (UsuarioVO enviada : enviadas.getUsuarios()) {
+	    			try {
+	        	        FXMLLoader fxmlLoader = new FXMLLoader(App.class.getResource("peticionItem.fxml"));
+	    	        	HBox salaItem = fxmlLoader.load();
+	    	        	
+	    	        	PeticionItemController peticionItemController = fxmlLoader.getController();
+	    	        	peticionItemController.setData(enviada, true);
+	    	        	
+	    	        	listaEnviadas.addRow(listaEnviadas.getRowCount(), salaItem);
+	        			
+	        			if (DEBUG) System.out.println("amigo encontrado:" + enviada.getCorreo());
+	    			} catch (IOException e) {
+	    				if (DEBUG) e.printStackTrace();
+	    			}
+	    		}
+	    	}
+    	});
 	}
 	
     @FXML
@@ -179,53 +178,49 @@ public class AmigosController implements Initializable{
 			if (DEBUG) System.out.println("Para buscar un amigo, introduce su correo");
 		} else {
 			//BUSCAR AMIGO
-			String sesionID = App.getSessionID();
 			
-			RestAPI apirest = new RestAPI("/api/buscarAmigo");
-			apirest.addParameter("sesionID", sesionID);
+			RestAPI apirest = App.apiweb.getRestAPI();
 			apirest.addParameter("amigo", correoAmigo);
 			apirest.setOnError(e -> {if (DEBUG) System.out.println(e);});
 	    	
-			apirest.openConnection();
-			ListaUsuarios usuarios = apirest.receiveObject(ListaUsuarios.class);
-			
-			//COMPROBAR SI HA HABIDO ALGÚN ERROR
-			String error = usuarios.getError();
-			if (error.equals("null")) {
-				
-				//EN CASO DE QUE LLEGUEN VARIOS (NUNCA DEBERÍA) SÓLO OBTENEMOS EL PRIMER USUARIO
-				UsuarioVO usuario = usuarios.getUsuarios().get(0);
-				if (usuarios.getUsuarios().size() > 1) {
-        			if (DEBUG) System.out.println("Revisar Backend. Se devuelve más de un usuario");
+			apirest.openConnection("/api/buscarAmigo");
+			apirest.receiveObject(ListaUsuarios.class, usuarios -> {
+				//COMPROBAR SI HA HABIDO ALGÚN ERROR
+				String error = usuarios.getError();
+				if (error.equals("null")) {
+					
+					//EN CASO DE QUE LLEGUEN VARIOS (NUNCA DEBERÍA) SÓLO OBTENEMOS EL PRIMER USUARIO
+					UsuarioVO usuario = usuarios.getUsuarios().get(0);
+					if (usuarios.getUsuarios().size() > 1) {
+	        			if (DEBUG) System.out.println("Revisar Backend. Se devuelve más de un usuario");
+					}
+					
+					try {
+	        	        FXMLLoader fxmlLoader = new FXMLLoader(App.class.getResource("amigoItem.fxml"));
+	    	        	HBox amigoItem = fxmlLoader.load();
+	    	        	
+	    	        	AmigoItemController amigoItemController = fxmlLoader.getController();
+	    	        	
+	    	        	//VERIFICAR SI ES AMIGO O NO
+	    	        	boolean esAmigo = false;
+	    	        	for (UsuarioVO u : listaAmigosLocal) {
+	    	        		esAmigo |= u.getCorreo().equals(usuario.getCorreo());
+	    	        	}
+	    	        	
+	    	        	amigoItemController.setData(usuario, esAmigo);
+	    	        	
+	        	        listaAmigos.addRow(listaAmigos.getRowCount(), amigoItem);
+	        			
+	        			if (DEBUG) System.out.println("amigo encontrado:" + usuario.getCorreo());
+	    			} catch (IOException e) {
+	    				if (DEBUG) e.printStackTrace();
+	    			}
+					
+				} else {
+					labelError.setText(StringUtils.parseString(error));
+					if (DEBUG) System.out.println(StringUtils.parseString(error));
 				}
-				
-				try {
-        	        FXMLLoader fxmlLoader = new FXMLLoader(App.class.getResource("amigoItem.fxml"));
-    	        	HBox amigoItem = fxmlLoader.load();
-    	        	
-    	        	AmigoItemController amigoItemController = fxmlLoader.getController();
-    	        	
-    	        	//VERIFICAR SI ES AMIGO O NO
-    	        	boolean esAmigo = false;
-    	        	for (UsuarioVO u : listaAmigosLocal) {
-    	        		esAmigo |= u.getCorreo().equals(usuario.getCorreo());
-    	        	}
-    	        	
-    	        	amigoItemController.setData(usuario, esAmigo);
-    	        	
-        	        listaAmigos.addRow(listaAmigos.getRowCount(), amigoItem);
-        			
-        			if (DEBUG) System.out.println("amigo encontrado:" + usuario.getCorreo());
-    			} catch (IOException e) {
-    				if (DEBUG) e.printStackTrace();
-    			}
-				
-			} else {
-				labelError.setText(StringUtils.parseString(error));
-				if (DEBUG) System.out.println(StringUtils.parseString(error));
-			}
+			});
 		}
     }
-
 }
-
